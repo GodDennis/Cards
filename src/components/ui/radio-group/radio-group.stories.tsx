78805827c04d1686b42598d1ef2055df3dc491b0
Artur <looppinooppi@gmail.com>
2024-02-13 14:22:47 +0300

    feat: component & style

--- src/components/ui/radio-group/radio-group.stories.tsx
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -3,6 +3,19 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { RadioGroup } from './radio-group'
 
 const meta = {
+  argTypes: {
+    changeHandler: {
+      action: 'clicked',
+    },
+  },
+  args: {
+    name: 'radio',
+    options: [
+      { default: true, value: 'a' },
+      { default: false, value: 'b' },
+      { default: false, value: 'c' },
+    ],
+  },
   component: RadioGroup,
   tags: ['autodocs'],
   title: 'Components/RadioGroup',
@@ -11,6 +24,10 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
-  args: {},
+export const Primary: Story = {}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
 }
--- src/components/ui/radio-group/radio-group.tsx
+++ src/components/ui/radio-group/radio-group.tsx
@@ -1,3 +1,55 @@
-export const RadioGroup = () => {
-  return <div></div>
+import * as RadioGr from '@radix-ui/react-radio-group'
+
+import s from './radio-group.module.scss'
+
+import { Typography } from '../typography'
+
+type Option = {
+  default: boolean
+  value: string
+}
+
+type RadioGroupProps = {
+  changeHandler: (value: string) => void
+  defaultValue?: string
+  disabled?: boolean
+  name: string
+  options: Option[]
+  required?: boolean
+}
+
+export const RadioGroup = (props: RadioGroupProps) => {
+  const { changeHandler, defaultValue, disabled, name, options } = props
+  const defaultOption = options.find(o => o.default)?.value
+  const classNames = {
+    radioGroupIndicator: s.radioGroupIndicator,
+    radioGroupItem: s.radioGroupItem,
+    radioGroupRoot: s.radioGroupRoot,
+    radioWrapper: s.radioWrapper,
+  }
+
+  return (
+    <RadioGr.Root
+      className={classNames.radioGroupRoot}
+      defaultValue={defaultValue || defaultOption}
+      disabled={disabled}
+      name={name}
+      onValueChange={changeHandler}
+    >
+      {options.map((o, i) => {
+        return (
+          <div className={classNames.radioWrapper} key={o.value}>
+            <RadioGr.Item className={classNames.radioGroupItem} id={i + o.value} value={o.value}>
+              <RadioGr.Indicator className={classNames.radioGroupIndicator} />
+            </RadioGr.Item>
+            <label htmlFor={i + o.value}>
+              <Typography as={'span'} variant={'body2'}>
+                {o.value}
+              </Typography>
+            </label>
+          </div>
+        )
+      })}
+    </RadioGr.Root>
+  )
 }
