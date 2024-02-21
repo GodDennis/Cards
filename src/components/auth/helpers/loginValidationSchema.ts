6a989363c11ec224ef5690176514221156839992
Artur <looppinooppi@gmail.com>
2024-02-21 14:03:59 +0300

    feat: email & password type added

--- src/components/auth/helpers/loginValidationSchema.ts
+++ src/components/auth/helpers/loginValidationSchema.ts
@@ -10,4 +10,14 @@ export const loginSchema = z.object({
   rememberMe: rememberMeZodCheck,
 })
 
+export const emailFormSchema = z.object({
+  email: emailZodCheck,
+})
+
+export const passwordFormSchems = z.object({
+  password: passwordZodCheck,
+})
+
 export type LoginFormValues = z.infer<typeof loginSchema>
+export type EmailFormValue = z.infer<typeof emailFormSchema>
+export type PasswordFormValue = z.infer<typeof passwordFormSchems>
