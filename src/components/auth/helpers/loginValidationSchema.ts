3d6a159197a23301d7a8103030178a827759b618
Artur <looppinooppi@gmail.com>
2024-02-26 11:56:33 +0300

    fix: prettier ignore line added

--- src/components/auth/helpers/loginValidationSchema.ts
+++ src/components/auth/helpers/loginValidationSchema.ts
@@ -12,16 +12,16 @@ export const loginSchema = z.object({
 
 export const registerSchema = z
   .object({
+    // eslint-disable-next-line prettier/prettier
+    confirmPassword: z.string().min(3).max(30),
     email: z.string().email(),
     password: z.string().min(3).max(30),
-    confirmPassword: z.string().min(3).max(30),
   })
   .refine(data => data.confirmPassword === data.password, {
     message: 'The passwords did not match',
     path: ['confirmPassword'],
   })
 
-export type FormValues = z.infer<typeof loginSchema>
 export type RegisterFormValues = z.infer<typeof registerSchema>
 
 export const emailFormSchema = z.object({
