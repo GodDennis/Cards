e947d366cc7e48911236e446bf7039ce23fdde8d
Artur <looppinooppi@gmail.com>
2024-02-26 13:27:19 +0300

    feat: in progress

--- src/layouts/question-card/QuestionCard.tsx
+++ src/layouts/question-card/QuestionCard.tsx
@@ -4,9 +4,11 @@ import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { RadioGroup } from '@/components/ui/radio-group'
 import { Typography } from '@/components/ui/typography'
-// mokcImg is temp
-import mokcImg from '@/layouts/images/Mask.png'
+// mockImg is temp
+import mockImg from '@/layouts/images/Mask.png'
 import clsx from 'clsx'
+
+import s from './question-card.module.scss'
 
 const radioOptions = [
   {
@@ -37,33 +39,35 @@ export const QuestionCard = () => {
   //temp
   const mockQuestion = 'How "This" works in JavaScript?'
   const mockAnswer = 'This is how "This" works in JavaScript'
-  const questionImg = mokcImg
-  const answerImg = mokcImg
+  const questionImg = mockImg
+  const answerImg = mockImg
 
   return (
     <Card>
       <div className={s.questionBlockWrapper}>
         <span className={s.questionStringkWrapper}>
           <Typography as={'span'} variant={'subtitle1'}>
-            Question:
+            {`Question: `}
           </Typography>
           <Typography as={'span'} variant={'body1'}>
             {mockQuestion}
           </Typography>
-          {questionImg && <img src={questionImg} />}
-          <Typography as={'span'}>Количество попыток ответов на вопрос: 10</Typography>
+          {questionImg && <img className={s.image} src={questionImg} />}
+          <Typography as={'span'} className={s.quectionNote} variant={'body2'}>
+            Количество попыток ответов на вопрос: 10
+          </Typography>
         </span>
       </div>
       <div className={s.answerBlockWrapper}>
         <span className={s.answerStringkWrapper}>
           <Typography as={'span'} variant={'subtitle1'}>
-            Answer:
+            {`Answer: `}
           </Typography>
           <Typography as={'span'} variant={'body1'}>
             {mockAnswer}
           </Typography>
         </span>
-        {answerImg && <img src={answerImg} />}
+        {answerImg && <img className={s.image} src={answerImg} />}
       </div>
       <RadioGroup
         changeHandler={setCurrentOption}
