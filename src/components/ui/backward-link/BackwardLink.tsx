2a1603c22ebd96738afd7aad9478088dfb99b0dc
Artur <looppinooppi@gmail.com>
2024-02-27 11:48:17 +0300

    feat: jsx created

--- src/components/ui/backward-link/BackwardLink.tsx
+++ src/components/ui/backward-link/BackwardLink.tsx
@@ -1,3 +1,9 @@
-export const BackwardLink = () => {
-  return <div></div>
+import { ElementType } from 'react'
+
+import { Typography, TypographyProps } from '@/components/ui/typography'
+
+type BackwardLinkProps<T extends ElementType> = TypographyProps<T>
+
+export const BackwardLink = <T extends ElementType>({ ...rest }: BackwardLinkProps<T>) => {
+  return <Typography {...rest} />
 }
