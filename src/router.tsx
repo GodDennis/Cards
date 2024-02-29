6350f03f6923a422172b57cb2a5d4edafb741608
Artur <looppinooppi@gmail.com>
2024-02-29 11:43:06 +0300

    feat: base router created

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,41 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
+
+import { SignIn } from './components/auth/signIn'
+import { DeskPage } from './pages/deskPage'
+
+const publicRouts: RouteObject[] = [
+  {
+    element: <SignIn />,
+    path: '/login',
+  },
+]
+
+const privatRouts: RouteObject[] = [
+  {
+    element: <DeskPage />,
+    path: '/',
+  },
+]
 
 const router = createBrowserRouter([
   {
-    element: <div>hello</div>,
-    path: '/',
+    children: privatRouts,
+    element: <PrivateRoutes />,
   },
+  ...publicRouts,
 ])
+
+function PrivateRoutes() {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
+}
 
 export const Router = () => {
   return <RouterProvider router={router} />
