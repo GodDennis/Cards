963bf5c9457ac3547ffa26b46615d3d2737b6a41
nina-was-not-available <ninamatushkina16@gmail.com>
2024-03-03 17:11:55 +0300

    fix: types decorator withRedux

--- src/utils/decorators/rtk-sb-dec.tsx
+++ src/utils/decorators/rtk-sb-dec.tsx
@@ -1,6 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {baseApi} from "@/services/base-api";
 import {Provider} from "react-redux";
+import {ReactNode} from "react";
 
 const sbStore = configureStore({
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
@@ -9,7 +10,7 @@ const sbStore = configureStore({
     },
 })
 
-export const withRedux = (Story: any) => (
+export const withRedux = (Story: () => ReactNode) => (
     <Provider store={sbStore}>
         <Story />
         </Provider>
