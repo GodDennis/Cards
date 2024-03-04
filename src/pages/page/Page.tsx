649ff4069bb3b856753e701ea57d251a0725a095
Artur <looppinooppi@gmail.com>
2024-03-04 11:31:58 +0300

    feat: add page

--- src/pages/page/Page.tsx
+++ src/pages/page/Page.tsx
@@ -1,3 +1,19 @@
-export const Page = () => {
-  return <div></div>
+import { ReactNode } from 'react'
+
+import { Header } from '@/components/ui/header'
+
+import s from './page.module.scss'
+
+type PageProps = {
+  children: ReactNode
+  isLoggedIn: boolean
+}
+
+export const Page = ({ children, isLoggedIn }: PageProps) => {
+  return (
+    <div className={s.pageWrapper}>
+      <Header isLoggedIn={isLoggedIn} />
+      <main className={s.main}>{children}</main>
+    </div>
+  )
 }
