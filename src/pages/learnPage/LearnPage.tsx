226e7b972b96c1380bf3fac86b70a71bf78f9715
Artur <looppinooppi@gmail.com>
2024-03-04 11:57:18 +0300

    feat: add jsx

--- src/pages/learnPage/LearnPage.tsx
+++ src/pages/learnPage/LearnPage.tsx
@@ -1,3 +1,18 @@
+import { Navigate, useParams } from 'react-router-dom'
+
+import { Page } from '../page'
+import { QuestionCard } from '../questionCard'
+
 export const LearnPage = () => {
-  return <div></div>
+  const { deckId } = useParams<string>()
+
+  if (deckId) {
+    return (
+      <Page isLoggedIn>
+        <QuestionCard deckId={deckId} />
+      </Page>
+    )
+  } else {
+    return <Navigate to={'/'} />
+  }
 }
