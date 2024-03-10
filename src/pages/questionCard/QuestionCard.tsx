49b3c1e8ed7a18ef7d1960d22e44626bd5cc3879
Artur <looppinooppi@gmail.com>
2024-03-10 16:37:26 +0300

    refactor: path canged

--- src/pages/questionCard/QuestionCard.tsx
+++ src/pages/questionCard/QuestionCard.tsx
@@ -66,7 +66,7 @@ export const QuestionCard = () => {
 
   return (
     <div className={s.pageContentWrapper}>
-      <BackwardLink className={s.backLink} to={'/decks-list'} variant={'body2'}>
+      <BackwardLink className={s.backLink} to={'/'} variant={'body2'}>
         Back to Decks List
       </BackwardLink>
       <Card className={s.questionCard}>
