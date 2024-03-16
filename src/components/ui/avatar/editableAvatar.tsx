3f6a40c86e314ae1c48063c50d292c8c51c6cf39
GodDennis <denpodreckiy@mail.ru>
2024-03-16 19:54:59 +0300

    feat: editProfile page

--- src/components/ui/avatar/editableAvatar.tsx
+++ src/components/ui/avatar/editableAvatar.tsx
@@ -1,23 +1,42 @@
+import { ChangeEvent, useRef } from 'react'
+
 import { EditPen } from '@/icons/EditPen'
 
 import s from './editableAvatar.module.scss'
 
 import { Button } from '../button'
+import { Input } from '../input'
 import { Avatar } from './Avatar'
 
 type Props = {
+  avatar: null | string
   className?: string
   isVisible?: boolean
+  onFile: (value: File) => void
+  onSubmit: (values: { avatar?: string; nickname?: string }) => void
 }
 
-export const EditableAvatar = ({ className, isVisible }: Props) => {
+export const EditableAvatar = ({ avatar, className, isVisible, onFile }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const onClick = () => {
+    inputRef.current?.click()
+  }
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.currentTarget.files?.[0] && onFile(e.currentTarget.files?.[0])
+  }
+
   return (
     <div className={`${s.container} ${className}`}>
-      <Avatar className={s.editAvatar} />
+      <Avatar className={s.editAvatar} src={avatar} />
       {isVisible && (
-        <Button className={s.editAvatarButton} variant={'secondary'}>
-          <EditPen />
-        </Button>
+        <div>
+          <Button className={s.editAvatarButton} onClick={onClick} variant={'secondary'}>
+            <EditPen />
+          </Button>
+          <Input className={s.inputFile} onChange={onChange} ref={inputRef} type={'file'} />
+        </div>
       )}
     </div>
   )
