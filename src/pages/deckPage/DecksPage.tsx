3808aea0529958a38f1e28398662fe9ff4a7fdb4
Artur <looppinooppi@gmail.com>
2024-03-18 17:15:24 +0300

    feat: implemented tab switcher

--- src/pages/deckPage/DecksPage.tsx
+++ src/pages/deckPage/DecksPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import { useDebounceValue } from '@/castomHooks/useDebounceValue'
 import { usePagination } from '@/castomHooks/usePagination'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -18,11 +19,20 @@ import { decksDto } from '@/utils/decksDto'
 import s from './deckPage.module.scss'
 
 export const DecksPage = () => {
+  const searchDefaultValue = ''
   const { currentPage, onSetCurrentPage, onSetPageSize, pageSize } = usePagination()
   const [isAddDeckOpen, setIsAddDeckOpen] = useState<boolean>(false)
+  const [tabValue, setTabValue] = useState<string>('allCards')
+  const [debouncedSearchStr, setDebouncedSearchStr] = useDebounceValue(searchDefaultValue, 500)
 
   const { data: userData } = useGetAuthQuery()
-  const { data } = useGetDecksQuery({ currentPage: currentPage, itemsPerPage: pageSize })
+  const { data } = useGetDecksQuery({
+    authorId: tabValue === 'myCards' ? userData.id : '',
+    currentPage: currentPage,
+    itemsPerPage: pageSize,
+    name: debouncedSearchStr,
+  })
+  const isSearchSuccessful = data?.items && data?.items.length > 0
 
   const tabs = [
     { name: 'My Cards', value: 'myCards' },
@@ -43,7 +53,8 @@ export const DecksPage = () => {
         <div className={s.deskActions}>
           <Input
             className={s.search}
-            onChange={() => { }}
+            defaultValue={searchDefaultValue}
+            onChange={e => setDebouncedSearchStr(e.currentTarget.value)}
             placeholder={'Input search'}
             variant={'search'}
           />
@@ -52,7 +63,7 @@ export const DecksPage = () => {
             <Typography className={s.tabLabel} variant={'body2'}>
               Show decks cards
             </Typography>
-            <TabSwitcher tabs={tabs}></TabSwitcher>
+            <TabSwitcher changeHandler={setTabValue} tabs={tabs} value={tabValue}></TabSwitcher>
           </div>
           <div className={s.flexItemsContainer}>
             <Typography className={s.sliderLabel} variant={'body2'}>
@@ -62,20 +73,30 @@ export const DecksPage = () => {
           </div>
           <Button variant={'secondary'}>Clear Filter</Button>
         </div>
-        <DescTable
-          authId={userData.id}
-          className={s.table}
-          decks={decksDto(data ?? ({} as GetDecksResponse))}
-          head={columns}
-        />
-        <Pagination
-          currentPage={currentPage}
-          pageSize={pageSize}
-          path={'decks'}
-          setCurrentPage={onSetCurrentPage}
-          setPageSize={onSetPageSize}
-          totalPages={data?.pagination.totalPages || 1}
-        />
+        {isSearchSuccessful ? (
+          <>
+            <DescTable
+              authId={userData.id}
+              className={s.table}
+              decks={decksDto(data ?? ({} as GetDecksResponse))}
+              head={columns}
+            />
+            <Pagination
+              currentPage={currentPage}
+              pageSize={pageSize}
+              path={'decks'}
+              setCurrentPage={onSetCurrentPage}
+              setPageSize={onSetPageSize}
+              totalPages={data?.pagination.totalPages || 1}
+            />
+          </>
+        ) : (
+          <Typography
+            as={'div'}
+            className={s.searchNotification}
+            variant={'body1'}
+          >{`No decks found with name "${debouncedSearchStr}"`}</Typography>
+        )}
       </div>
       <AddNewDeck closeHandler={setIsAddDeckOpen} open={isAddDeckOpen} />
     </div>
