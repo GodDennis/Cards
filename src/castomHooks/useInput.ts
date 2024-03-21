a66ce17b578303f3bde42535aa711731be802699
GodDennis <denpodreckiy@mail.ru>
2024-03-21 13:26:42 +0300

    refactor: rename search value

--- src/castomHooks/useInput.ts
+++ src/castomHooks/useInput.ts
@@ -6,20 +6,20 @@ import { useDebounceValue } from './useDebounceValue'
 export const useInput = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const [debouncedSearchStr, setDebouncedSearchStr] = useDebounceValue(
-    searchParams.get('val') ?? '',
+    searchParams.get('search') ?? '',
     500
   )
 
   const URLParams = Object.fromEntries(searchParams)
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const inputQuery = { val: e.currentTarget.value.toString() }
+    const inputQuery = { search: e.currentTarget.value.toString() }
 
     delete URLParams?.page
     setDebouncedSearchStr(e.currentTarget.value.toString() ?? '')
     setSearchParams({ ...URLParams, ...inputQuery })
     if (e.currentTarget.value.toString() === '') {
-      delete URLParams.val
+      delete URLParams.search
       setSearchParams({ ...URLParams })
     }
   }
